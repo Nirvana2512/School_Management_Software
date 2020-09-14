91dafa7fca88df9ea2ad5624e29657e37e53c1d8
Nirvana2512 <63945015+Nirvana2512@users.noreply.github.com>
2020-09-14 15:58:22 +0530

    Add files via upload

---
+++ USERS.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NULL PRIMARY KEY IDENTITY, 
+    [Username] NVARCHAR(50) NULL, 
+    [Password] NVARCHAR(50) NULL, 
+    [Fullname] NVARCHAR(50) NULL, 
+    [Picture] NVARCHAR(500) NULL
+)
